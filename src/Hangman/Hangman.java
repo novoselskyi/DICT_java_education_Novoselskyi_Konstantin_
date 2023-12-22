1041910773d67b1dcdd403836981b9a08856a803
novoselskyi <k.d.novoselskyi@student.khai.edu>
2023-12-22 22:36:51 +0200

    Hangman 2-nd stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,2 +1,29 @@
-package Hangman;public class Hangman {
+// Hangman.java
+package hangman;
+
+import java.util.Scanner;
+
+public class Hangman {
+    public static void main(String[] args) {
+        System.out.println("HANGMAN");
+
+        // Слово, яке гравець повинен вгадати
+        String secretWord = "java";
+
+        // Вивід вказівки для гравця
+        System.out.print("Guess the word: > ");
+
+        // Створення об'єкта Scanner для зчитування введення користувача
+        Scanner scanner = new Scanner(System.in);
+
+        // Зчитування введеного слова від гравця
+        String playerGuess = scanner.nextLine();
+
+        // Перевірка, чи вгадане слово вірне
+        if (playerGuess.equalsIgnoreCase(secretWord)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
+    }
 }
