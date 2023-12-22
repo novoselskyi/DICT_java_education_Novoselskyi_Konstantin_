142fb3fe72a3eb1f5234287e3733d481ee51933f
novoselskyi <k.d.novoselskyi@student.khai.edu>
2023-12-23 00:29:37 +0200

    CreditCalculator 3-rd stage

--- src/CreditCalculator/CreditCalculator.java
+++ src/CreditCalculator/CreditCalculator.java
@@ -7,51 +7,99 @@ public class CreditCalculator {
         Scanner scanner = new Scanner(System.in);
 
         // Вхідні дані
-        System.out.println("Enter the loan principal:");
-        double loanPrincipal = scanner.nextDouble();
-
         System.out.println("What do you want to calculate?\n" +
-                "type \"m\" – for the number of monthly payments,\n" +
-                "type \"p\" – for the monthly payment:");
+                "type \"n\" for the number of monthly payments,\n" +
+                "type \"a\" for annuity monthly payment amount,\n" +
+                "type \"p\" for loan principal:");
         char choice = scanner.next().charAt(0);
 
+        double loanPrincipal = 0;
         double monthlyPayment = 0;
-        double lastPayment = 0;
+        double loanInterest = 0;
+        int numberOfPayments = 0;
 
-        if (choice == 'm') {
-            // Розрахунок кількості місяців
-            System.out.println("Enter the monthly payment:");
-            monthlyPayment = scanner.nextDouble();
-            lastPayment = calculateLastPayment(loanPrincipal, monthlyPayment);
-            int numberOfMonths = calculateNumberOfMonths(loanPrincipal, monthlyPayment);
-            System.out.println("It will take " + numberOfMonths + " months to repay the loan");
-        } else if (choice == 'p') {
-            // Розрахунок щомісячного платежу
-            System.out.println("Enter the number of months:");
-            int numberOfMonths = scanner.nextInt();
-            monthlyPayment = calculateMonthlyPayment(loanPrincipal, numberOfMonths);
-            lastPayment = calculateLastPayment(loanPrincipal, monthlyPayment);
-            System.out.println("Your monthly payment = " + Math.ceil(monthlyPayment));
-        }
+        // Обчислення параметрів кредиту
+        switch (choice) {
+            case 'n':
+                // Розрахунок кількості місяців
+                System.out.println("Enter the loan principal:");
+                loanPrincipal = scanner.nextDouble();
+                System.out.println("Enter the monthly payment:");
+                monthlyPayment = scanner.nextDouble();
+                System.out.println("Enter the loan interest:");
+                loanInterest = scanner.nextDouble();
+                numberOfPayments = calculateNumberOfPayments(loanPrincipal, monthlyPayment, loanInterest);
+                printNumberOfPayments(numberOfPayments);
+                break;
 
-        // Перевірка останнього платежу
-        if (lastPayment != monthlyPayment) {
-            System.out.println("and the last payment = " + (int) lastPayment + ".");
+            case 'a':
+                // Розрахунок щомісячного платежу
+                System.out.println("Enter the loan principal:");
+                loanPrincipal = scanner.nextDouble();
+                System.out.println("Enter the number of periods:");
+                numberOfPayments = scanner.nextInt();
+                System.out.println("Enter the loan interest:");
+                loanInterest = scanner.nextDouble();
+                monthlyPayment = calculateAnnuityPayment(loanPrincipal, numberOfPayments, loanInterest);
+                printMonthlyPayment(monthlyPayment);
+                break;
+
+            case 'p':
+                // Розрахунок основної суми кредиту
+                System.out.println("Enter the annuity payment:");
+                monthlyPayment = scanner.nextDouble();
+                System.out.println("Enter the number of periods:");
+                numberOfPayments = scanner.nextInt();
+                System.out.println("Enter the loan interest:");
+                loanInterest = scanner.nextDouble();
+                loanPrincipal = calculateLoanPrincipal(monthlyPayment, numberOfPayments, loanInterest);
+                printLoanPrincipal(loanPrincipal);
+                break;
+
+            default:
+                System.out.println("Invalid choice. Please choose 'n', 'a', or 'p'.");
         }
     }
 
     // Метод для обчислення кількості місяців
-    public static int calculateNumberOfMonths(double principal, double monthlyPayment) {
-        return (int) Math.ceil(principal / monthlyPayment);
+    public static int calculateNumberOfPayments(double principal, double monthlyPayment, double interest) {
+        double i = interest / 100 / 12;
+        return (int) Math.ceil(Math.log(monthlyPayment / (monthlyPayment - i * principal)) / Math.log(1 + i));
     }
 
     // Метод для обчислення щомісячного платежу
-    public static double calculateMonthlyPayment(double principal, int numberOfMonths) {
-        return principal / numberOfMonths;
+    public static double calculateAnnuityPayment(double principal, int numberOfPayments, double interest) {
+        double i = interest / 100 / 12;
+        return principal * (i * Math.pow(1 + i, numberOfPayments)) / (Math.pow(1 + i, numberOfPayments) - 1);
     }
 
-    // Метод для обчислення останнього платежу
-    public static double calculateLastPayment(double principal, double monthlyPayment) {
-        return principal - (calculateNumberOfMonths(principal, monthlyPayment) - 1) * monthlyPayment;
+    // Метод для обчислення основної суми кредиту
+    public static double calculateLoanPrincipal(double monthlyPayment, int numberOfPayments, double interest) {
+        double i = interest / 100 / 12;
+        return monthlyPayment / ((i * Math.pow(1 + i, numberOfPayments)) / (Math.pow(1 + i, numberOfPayments) - 1));
+    }
+
+    // Метод для виведення кількості місяців
+    public static void printNumberOfPayments(int numberOfPayments) {
+        int years = numberOfPayments / 12;
+        int months = numberOfPayments % 12;
+
+        if (years > 0 && months > 0) {
+            System.out.println("It will take " + years + " years and " + months + " months to repay this loan!");
+        } else if (years > 0) {
+            System.out.println("It will take " + years + " years to repay this loan!");
+        } else {
+            System.out.println("It will take " + months + " months to repay this loan!");
+        }
+    }
+
+    // Метод для виведення щомісячного платежу
+    public static void printMonthlyPayment(double monthlyPayment) {
+        System.out.println("Your monthly payment = " + Math.ceil(monthlyPayment) + "!");
+    }
+
+    // Метод для виведення основної суми кредиту
+    public static void printLoanPrincipal(double loanPrincipal) {
+        System.out.println("Your loan principal = " + (int) Math.ceil(loanPrincipal) + "!");
     }
 }
