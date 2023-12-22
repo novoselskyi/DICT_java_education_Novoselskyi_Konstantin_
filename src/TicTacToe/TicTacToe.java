e1bc6485d7de86d9bb1e67475b84ca4906e27c29
novoselskyi <k.d.novoselskyi@student.khai.edu>
2023-12-22 23:50:31 +0200

    TicTacToe 2-nd stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,2 +1,39 @@
-package TicTacToe;public class TicTacToe {
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        // Етап 2: Зчитати і відобразити стан гри
+        readAndDisplayGameState();
+    }
+
+    static void readAndDisplayGameState() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter cells: ");
+        String input = scanner.nextLine();
+
+        // Перевірка довжини введеного рядка
+        if (input.length() != 9) {
+            System.out.println("Error: Invalid input length. Please enter exactly 9 characters.");
+            return;
+        }
+
+        // Перетворення рядка в масив символів для відображення
+        char[] gameState = input.toCharArray();
+
+        // Виведення тире та верхньої границі ігрового поля
+        System.out.println("---------");
+
+        // Виведення ігрового поля
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(gameState[i * 3 + j] + " ");
+            }
+            System.out.println("|");
+        }
+
+        // Виведення тире та нижньої границі ігрового поля
+        System.out.println("---------");
+    }
 }
